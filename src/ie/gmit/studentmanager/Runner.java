381765ff4896bd53420d6d62468a9daa19f393b5
danielcregg <daniel.cregg@gmail.com>
2018-10-26 17:51:34 +0100

    Rename Buttons

--- src/ie/gmit/studentmanager/Runner.java
+++ src/ie/gmit/studentmanager/Runner.java
@@ -16,14 +16,14 @@ public class Runner extends Application {
 	public void start(Stage primaryStage) {
 
 		Text myText = new Text("Please select a Menu Option below:");
-		Button button1 = new Button("Button 1");
-		Button button2 = new Button("Button 2");
-		Button button3 = new Button("Button 3");
-		Button button4 = new Button("Button 4");
-		Button button5 = new Button("Button 5");
-		Button button6 = new Button("Button 6");
-		Button button7 = new Button("Button 7");
-		Button button8 = new Button("Button 8");	
+		Button buttonLoadDB = new Button("Load DB");
+		Button buttonAdd = new Button("Add Student");
+		Button buttonDelete = new Button("Delete Student");
+		Button buttonSearchByID = new Button("Search by ID");
+		Button buttonSearchByFirstName = new Button("Search by First Name");
+		Button buttonShowTotal = new Button("Show Total Students");
+		Button buttonSaveDB = new Button("Save DB");
+		Button buttonQuit = new Button("Quit");	
 		TextArea myOutput = new TextArea();
 
 		GridPane myGridPane = new GridPane();
@@ -40,14 +40,14 @@ public class Runner extends Application {
 
 		// Arranging all the nodes in the grid
 		myGridPane.add(myText, 0, 0);
-		myGridPane.add(button1, 0, 1);
-		myGridPane.add(button2, 0, 2);
-		myGridPane.add(button3, 0, 3);
-		myGridPane.add(button4, 0, 4);
-		myGridPane.add(button5, 0, 5);
-		myGridPane.add(button6, 0, 6);
-		myGridPane.add(button7, 0, 7);
-		myGridPane.add(button8, 0, 8);
+		myGridPane.add(buttonLoadDB, 0, 1);
+		myGridPane.add(buttonAdd, 0, 2);
+		myGridPane.add(buttonDelete, 0, 3);
+		myGridPane.add(buttonSearchByID, 0, 4);
+		myGridPane.add(buttonSearchByFirstName, 0, 5);
+		myGridPane.add(buttonShowTotal, 0, 6);
+		myGridPane.add(buttonSaveDB, 0, 7);
+		myGridPane.add(buttonQuit, 0, 8);
 		myGridPane.add(myOutput, 0, 9);
 
 		/* Preparing the Scene */
