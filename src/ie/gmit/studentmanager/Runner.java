3541af3dc12ffb7724d6b1991dfa0d3cf23da864
Zoe Morgan <g00346934@gmit.ie>
2018-11-08 17:36:21 +0000

    add textfields and event when add, delete and total are clicked

--- src/ie/gmit/studentmanager/Runner.java
+++ src/ie/gmit/studentmanager/Runner.java
@@ -6,28 +6,58 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Runner extends Application {
-
+	
+	StudentManager sm = new StudentManager();
+	//Scene scene1;
+	//Scene scene2;
+	//GridPane gridPane1;
+	//VBox vBox1;
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 
 		Text myText = new Text("Please select a Menu Option below:");
 		Button buttonLoadDB = new Button("Load DB");
-		Button buttonAdd = new Button("Add Student");
-		Button buttonDelete = new Button("Delete Student");
 		Button buttonSearchByID = new Button("Search by ID");
 		Button buttonSearchByFirstName = new Button("Search by First Name");
-		Button buttonShowTotal = new Button("Show Total Students");
 		Button buttonSaveDB = new Button("Save DB");
 		Button buttonQuit = new Button("Quit");	
 		TextArea myOutput = new TextArea();
+		TextField textDB = new TextField("Load DB");
+		TextField textSearchID = new TextField("Enter ID to Search");
+		TextField textSearchFname = new TextField("Enter First name to search");
 
 		GridPane myGridPane = new GridPane();
-
+		
+		//add Student button & event
+		Button buttonAdd = new Button("Add Student");
+		TextField textAdd = new TextField("Enter Student ID");
+		buttonAdd.setOnAction(event -> {
+			sm.add(new Student(textAdd.getText()));
+		});
+		
+		//delete button and event
+		Button buttonDelete = new Button("Delete Student");
+		TextField textDelete = new TextField("Enter ID to delete");
+		buttonDelete.setOnAction(e -> {
+			sm.delete(textDelete.getText());
+		});
+		
+		//show total button & event
+		Button buttonShowTotal = new Button("Show Total Students");
+		TextField textTotal = new TextField("Total Students");
+		buttonShowTotal.setOnAction(e -> {
+			textTotal.setText(Integer.toString(sm.findTotalStudents()));
+		});
+		
 		// Setting the padding
 		myGridPane.setPadding(new Insets(10, 10, 10, 10));
 
@@ -41,14 +71,21 @@ public class Runner extends Application {
 		// Arranging all the nodes in the grid
 		myGridPane.add(myText, 0, 0);
 		myGridPane.add(buttonLoadDB, 0, 1);
+		myGridPane.add(textDB, 1, 1);
 		myGridPane.add(buttonAdd, 0, 2);
+		myGridPane.add(textAdd, 1, 2);
 		myGridPane.add(buttonDelete, 0, 3);
+		myGridPane.add(textDelete, 1, 3);
 		myGridPane.add(buttonSearchByID, 0, 4);
+		myGridPane.add(textSearchID, 1, 4);
 		myGridPane.add(buttonSearchByFirstName, 0, 5);
+		myGridPane.add(textSearchFname, 1, 5);
 		myGridPane.add(buttonShowTotal, 0, 6);
+		myGridPane.add(textTotal, 1, 6);
 		myGridPane.add(buttonSaveDB, 0, 7);
 		myGridPane.add(buttonQuit, 0, 8);
 		myGridPane.add(myOutput, 0, 9);
+
 
 		/* Preparing the Scene */
 		// Create a Scene by passing the root group object, height and width
